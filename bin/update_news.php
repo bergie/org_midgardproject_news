0c3ae541d7cf1443d2390bb880810d29a56e4389
Henri Bergius <henri.bergius@iki.fi>
2011-01-27 17:39:57 +0200

    Open connection before import

--- bin/update_news.php
+++ bin/update_news.php
@@ -1,4 +1,10 @@
 <?php
+$filepath = ini_get ("midgard.configuration_file");
+$config = new midgard_config ();
+$config->read_file_at_path($filepath);
+$mgd = midgard_connection::get_instance();
+$mgd->open_config($config); 
+
 $basedir = dirname(__FILE__) . '/../..';
 require("{$basedir}/midgardmvc_core/framework.php");
 $mvc = midgardmvc_core::get_instance("{$basedir}/application.yml");
